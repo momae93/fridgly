114277476dfe80e03a4c906c6fdc18d7153188d3
Mickael AU <mickael.au@gmail.com>
2019-01-01 14:09:22 +0100

    [NAVIGATION][LOGO] Changed navigations icons + logo

--- app/src/main/java/com/au/fridgly/MainActivity.kt
+++ app/src/main/java/com/au/fridgly/MainActivity.kt
@@ -9,16 +9,16 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_trophy -> {
+                message.setText(R.string.title_trophy)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_search -> {
+                message.setText(R.string.title_search)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_favorite -> {
+                message.setText(R.string.title_favorite)
                 return@OnNavigationItemSelectedListener true
             }
         }
