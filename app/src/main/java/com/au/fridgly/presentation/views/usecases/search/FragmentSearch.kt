03c676e6ca2f312d0598aad3aa28e53b4f68be9e
Mickael AU <mickael.au@gmail.com>
2019-01-01 23:52:49 +0100

    [SEARCH][UI] Added custom searchview

--- app/src/main/java/com/au/fridgly/presentation/views/usecases/search/FragmentSearch.kt
+++ app/src/main/java/com/au/fridgly/presentation/views/usecases/search/FragmentSearch.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.SearchView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +20,7 @@ class FragmentSearch : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
+        val searchView: SearchView = view.findViewById(R.id.fragment_search_searchView_search)
         return view
     }
 
